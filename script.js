5f38cad12b9ea55a766a8ab9b5762040aad30c03
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-07-25 19:37:49 +0100

    updated else if statements, added retrun output

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ var upperCasedCharacters = [
   "Z",
 ];
 
-//What does this bit do?
+//Leave this bit?
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -112,6 +112,14 @@ passCriteria = {
   upperCasedCharacters,
 };
 
+var userLength = 0;
+var userSpecial;
+var userUpper;
+var userLower;
+var userNumber;
+output = "";
+passCriteria.passLength = 0;
+
 // Function to generate password with user input - THE VARIABLES ARE WHAT THE USER HAS ENTERED
 function generatePassword() {
   var userLength = 0;
@@ -128,7 +136,7 @@ function generatePassword() {
   if (
     confirm(
       "The fun is about to start! In order to create your password, we'll need you to choose how many characters you'd like it to be. Click 'OK' to continue."
-    ) == false
+    ) === null
   ) {
     alert("We're sad to see you leave, but you're welcome back anytime!");
     return "Click below to generate your password";
@@ -138,61 +146,77 @@ function generatePassword() {
 
   // Make sure the number the user enters is a) between 8 and 128 and b) a number and not anything else
   // If user enters correctly, let them proceed to the next options.
-  if (userLength > 7 && userLength < 129) {
+  if (userLength > 8 && userLength < 128) {
     passwordOptions();
 
     //If user doesn't enter correctly, prompt them to try again
-  } else if (userLength < 7 || userLength > 129) {
+  } else if ((userLength = null)) {
+    alert("We're sad to see you leave, but you're welcome back anytime!");
+    return "Click below to generate your password";
+  } else if (typeof userLength !== "number") {
+    prompt(
+      "Good idea, but we're looking for a number (e.g. 1, 2, 3) between 8 and 128. Please try again."
+    );
+  } else {
     prompt(
       "Close! But the number needs to be between 8 and 128. Please try again."
     );
-  } else {
-    userLength =
-      specialCharacters || lowerCasedCharacters || upperCasedCharacters;
   }
-  prompt(
-    "Good idea, but we're looking for a number (e.g. 1, 2, 3) between 8 and 128. Please try again."
-  );
-  if (userLength === null) {
-    alert("We're sad to see you leave, but you're welcome back anytime!");
-    return "Click below to generate your password";
-  }
-}
-
-// Give the users password prompts
-function passwordOptions() {
-  userSpecial = confirm(
-    "Excellent work. Now, since you're so special, would you like to include a special character? E.g. $*%."
-  );
-  userUpper = confirm(
-    "Next up(per)...capital letters! Would you like to include one?"
-  );
-  userLower = confirm(
-    "How about a lowercase letter? We hear they're all the rage..."
-  );
-  userNumber = confirm(
-    "What about a number? We're not trying to flirt, honestly!"
-  );
-  //Get the password to show up in the box
-  return outcome;
-}
-
-//Continue the prompt box journey based on users' responses
-//First of all, need to sort out the password length, making sure the password generator has at least one variable to choose from.
-while (passCriteria.passLength < userLength) {
-  if (
-    userSpecial === false &&
-    userUpper === false &&
-    userLower === false &&
-    userNumber === false
-  ) {
-    alert(
-      "Your password is depending on you! To any further, you must select at least one type of character out of numbers, lowercase, uppercase and special characters."
+  // Give the users password prompts
+  function passwordOptions() {
+    userSpecial = confirm(
+      "Excellent work. Now, since you're so special, would you like to include a special character? E.g. $*%."
     );
-    passwordOptions();
+    userUpper = confirm(
+      "Next up(per)...capital letters! Would you like to include one?"
+    );
+    userLower = confirm(
+      "How about a lowercase letter? We hear they're all the rage..."
+    );
+    userNumber = confirm("What about a number? 😉");
   }
-  //To ensure that the password has at least one of the characters the user has specifically selected, make sure the loop grabs at least one special character/ uppercase/ lowercase / number from the  and adds it to the end
-  else {
-    if 
+
+  //Continue the prompt box journey based on users' responses
+  //First of all, need to sort out the password length, making sure the password generator has at least one variable to choose from.
+  while (passCriteria.passLength < userLength) {
+    if (
+      userSpecial === false &&
+      userUpper === false &&
+      userLower === false &&
+      userNumber === false
+    ) {
+      alert(
+        "Your password is depending on you! To go any further, you must select at least one type of character out of numbers, lowercase, uppercase and special characters."
+      );
+      passwordOptions();
+    }
+    //To ensure that the password has at least one of the characters the user has specifically selected, make sure the loop grabs at least one special character/ uppercase/ lowercase / number from the  and adds it to the end
+    else {
+      if (userSpecial === true && passCriteria.passLength < userLength) {
+        var spec =
+          passCriteria.specialCharacters[Math.floor(Math.random() * 23)];
+        output = output + spec;
+        passCriteria.passLength++;
+      }
+      if (userSpecial === true && passCriteria.passLength < userLength) {
+        var up =
+          passCriteria.upperCasedCharacters[Math.floor(Math.random() * 26)];
+        output = output + up;
+        passCriteria.passLength++;
+      }
+      if (userLower === true && passCriteria.passLength < userLength) {
+        var low =
+          passCriteria.lowerCasedCharacters[Math.floor(Math.random() * 26)];
+        output = output + low;
+        passCriteria.passLength++;
+      }
+      if (userNumber === true && passCriteria.passLength < userLength) {
+        var num =
+          passCriteria.numericCharacters[Math.floor(Math.random() * 10)];
+        output = output + num;
+        passCriteria.passLength++;
+      }
+    }
+    return output;
   }
 }
